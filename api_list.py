a4b0bf6329ffc8d309768fcc3b7982baf50b8794
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2023-02-20 01:56:02 +0900

    Update api_list.py

--- api_list.py
+++ api_list.py
@@ -1,2 +1,2 @@
 DEV_API_LIST = ["http://127.0.0.1:8002", "http://127.0.0.1:8003"]
-PRD_API_LIST = ["https://j7bizt.deta.dev", "https://1ydi3z.deta.dev"]
+PRD_API_LIST = ["ttps://blockchain-api-2.onrender.com", "https://blockchain-api-3.onrender.com"]
